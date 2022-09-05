11582dba127a1d3fa80520d6f84a83368b7f5066
wand3 <godswillbaiyewu@gmail.com>
2022-09-05 13:48:11 -0700

    shell.h update

--- shell.h
+++ shell.h
@@ -89,8 +89,8 @@ typedef struct passinfo
 	int env_changed;
 	int status;
 
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-[O	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement*/
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
 } info_t;
